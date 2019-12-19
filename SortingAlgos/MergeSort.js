b403cf5f941e8cab6bb8a7f39d210a590371eb7e
tkmoney11 <kimt29@uw.edu>
2019-12-19 15:19:42 -0800

    Added full comments to selectionSort. Added partial comments to mergeSort.

---
+++ SortingAlgos/MergeSort.js
@@ -0,0 +1,34 @@
+function mergeSort(arr) {}
+   let n = arr.length                  // length of input array
+   let left[];                         // left sub-array
+   let right[];                        // right sub-array
+
+   // create space for left and right sub-arrays
+   if (n == 0) {                       // if array is empty, return empty array
+      return arr;
+   } else if (n % 2 == 0) {            // if array length is even, sub-arrays are half of length
+      left = new int[n / 2];
+      right = new int[n / 2];
+   } else {                            // if array length is odd, right array is 1 bigger than left
+      left = new int[n / 2];
+      right = new int[n / 2 + 1];
+   }
+
+   // index cursors
+   let i = 0;                          // index cursor for left sub-array
+   let j = 0;                          // index cursor for right sub-array
+
+   // fill in left and right sub-arrays (unsorted)
+   if (n % 2 == 0) {                   // if array is empty, fill
+      
+   } else {
+
+   }
+   // recursively split and sort arrays
+   // return a merge left and right subarrays
+}
+
+// function for split
+
+// function for merge sorted arrays
+// create space for result arrays
--- SortingAlgos/SelectionSort.js
+++ SortingAlgos/SelectionSort.js
@@ -1,13 +1,17 @@
 function selectionSort(arr) {
    for (let i = 0; i < arr.length - 1; i++) {
-      let min_index = i;
-      for (let j = i + 1; j < arr.length; j++) {
-         if (arr[j] < arr[min_index]) {
+      let min_index = i;                           // set index of min to first element of sub-array.
+
+      // find min element in sub-array
+      for (let j = i + 1; j < arr.length; j++) {   // iterate over 2nd element of sub-array to end of array
+         if (arr[j] < arr[min_index]) {            // check if current element is smaller than min.
             min_index = j;
          }
       }
-      let min = arr[min_index];
-      arr[min_index] = arr[i];
-      arr[i] = min;
+
+      // swap min and first elements
+      let min = arr[min_index];                    // placeholder for min value
+      arr[min_index] = arr[i];                     // first element into index of min element
+      arr[i] = min;                                // min value into first element
    }
 }
